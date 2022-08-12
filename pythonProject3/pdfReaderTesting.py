3d4e7115b1fffc05ac8aa9514a6f35de8ad846bf
ColteRodriguez <111135624+ColteRodriguez@users.noreply.github.com>
2022-08-12 11:17:24 -0400

    Update pdfReaderTesting.py

--- pythonProject3/pdfReaderTesting.py
+++ pythonProject3/pdfReaderTesting.py
@@ -2,6 +2,33 @@
 import PyPDF2
 import os
 import xlsxwriter
+from tkinter import*
+import tkinter as tk
+
+py = []
+master = Tk()
+master.title("CilasPal: Grain Size Digitizer")
+master.geometry("450x250")
+
+e = Entry(master)
+e.pack()
+e.focus_set()
+var = StringVar()
+label = Label(master, textvariable=var, relief=RAISED, wraplength=400)
+
+var.set("Please paste the directory from which you would like to retrieve data by doing the following 1. Navigate to the folder of the cilas files on this computer (this will most likely be located on an external drive) 2. In the file destination field (the space to the left of the file search field), left click once to highlight the directory 3. Copy and paste here and click OK")
+label.pack()
+
+
+def callback():
+    repo = e.get() # This is the text you may want to use later
+    print("retrieving data from repository...", repo)
+    py.append(repo)
+    master.destroy()
+
+b = Button(master, text = "OK", width = 10, command = callback)
+b.pack()
+mainloop()
 
 #  the initialization components of the spreadsheet to sort inserted data
 insList = ["ID", "Mean", "Median", 0.04, 0.07, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1,
@@ -13,10 +40,11 @@ insList = ["ID", "Mean", "Median", 0.04, 0.07, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7
            300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0, 1100.0, 1200.0, 1300.0,
            1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0, 2300.0,
            2400.0, 2500.0]
-insList2 = [0.04, 3.90, 62.00, 88.00, 125.00, 177.0, 250.0, 350.0, 500.0, 710.0, 1000.0, 1410.0, 2000.0]
+#creating a list for UDSC
+insList2 = [0.04, 3.90, 62.00, 88.00, 125.00, 177.0, 2500.0, 350.0, 500.0, 710.0, 1000.0, 1410.0, 2000.0]
 
 # Creates a workbook in excel to house all the data
-workbook = xlsxwriter.Workbook('C:/Users/Tony/Dropbox/PythonScreenShotTest/New Folder/YourData.xlsx')
+workbook = xlsxwriter.Workbook(py[0] + '//YourData.xlsx')
 worksheet = workbook.add_worksheet("Primary Data Tables")
 worksheet2 = workbook.add_worksheet("User Defined Size Classes")
 
@@ -26,24 +54,26 @@ for i in range(103):
 for i in range(13):
     worksheet2.write(0, i + 1, insList2[i])
 
-repo = r'C:\Users\Tony\Dropbox\PythonScreenShotTest'
+# why wont this work repo = input("Paste Your Repository Here: ") --> becasue using os.listdir and not open()?
+# repo = input("Please input the repository of your cilas data here: ")
+
 print("\033[1;31mRepository received  \n" )
 print("Error:GetFileNotFound IGNORED")
 pdfsList = []
 
 # 1.Get file names from directory
-file_list = os.listdir(repo)
+file_list = os.listdir(py[0])
 
 # 2. generates a list of all the pdfs in the repository
 for i in range(len(file_list)):
     file = file_list[i]
     if file.__contains__('.pdf'):
         pdfsList.append(file)
-
+print(pdfsList)
 
 for p in range(len(pdfsList)):
     #   creating a pdf file object
-    pdfFileObj = open(repo + '/' + pdfsList[p], 'rb')
+    pdfFileObj = open(py[0] + '/' + pdfsList[p], 'rb')
 
     # creating a pdf reader object
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -64,14 +94,14 @@ for p in range(len(pdfsList)):
     startIndex = Index + 19
     endIndex = startIndex + 6
 
-    # Data from the USer defined size classes
+    # Extracting the User defined size classes
     for i in range(4):
-        bigQArray.append(List2[startIndex: endIndex])
+        bigQArray.append(List2[startIndex:endIndex])
         startIndex = startIndex + 13
         endIndex = endIndex + 13
 
     startIndex = startIndex - 1
-    endIndex = endIndex - 1
+    endIndex = endIndex - 1  
     for i in range(6):
         bigQArray.append(List2[startIndex:endIndex])
         startIndex = startIndex + 12
@@ -80,11 +110,12 @@ for p in range(len(pdfsList)):
     startIndex = startIndex + 5
     endIndex = endIndex + 5
     for i in range(3):
-        bigQArray.append(List2[startIndex: endIndex])
+        bigQArray.append(List2[startIndex:endIndex])
         startIndex = startIndex + 13
         endIndex = endIndex + 13
 
-    # Data from the full size class
+
+    # Extracting the Full Size Class
     sIndex = List.index('undersize')+ 28
     eIndex = sIndex + 6
     for i in range(6):
@@ -157,3 +188,16 @@ for p in range(len(pdfsList)):
     # closing the pdf file object
     pdfFileObj.close()
 workbook.close()
+
+# Notify the user when the program finishes running
+final = Tk()
+final.title("CilasPal: Grain Size Digitizer")
+final.geometry("450x250")
+
+
+var = StringVar()
+label = Label(final, textvariable=var, relief=RAISED, wraplength=400)
+
+var.set("Your Excel file (YourData.xlsx) has been created. You may view your data in the previously input file loaction. Close this window to close the app")
+label.pack()
+mainloop()
